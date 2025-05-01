fd9729089f55a93c9085f3ade7f38d3146711db0
diana <diana.frame17@vk.com>
2025-05-01 15:24:20 +0300

    поменяла иконку у приложения

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.viewanddeletecontactsapp"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.viewanddeletecontactsapp"
